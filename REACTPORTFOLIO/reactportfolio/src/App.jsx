dc71fb9c0abdc2c4e7cf35105addbb64c9ec2f43
Amethnb2218 <amethsl2218@gmail.com>
2026-08-04 02:16:04 +0000

    Fix navigation SPA : wrapper motion.div pour AnimatePresence compatible React Router v7

--- REACTPORTFOLIO/reactportfolio/src/App.jsx
+++ REACTPORTFOLIO/reactportfolio/src/App.jsx
@@ -149,13 +149,21 @@ function App() {
       <Scene3D />
       <Navbar />
       <AnimatePresence mode="wait">
-        <Routes location={location} key={location.pathname}>
-          <Route path="/" element={<Home />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/projects/:slug" element={<ProjectDetail />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/contact" element={<Contact />} />
-        </Routes>
+        <motion.div
+          key={location.pathname}
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={{ duration: 0.3 }}
+        >
+          <Routes location={location}>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects />} />
+            <Route path="/projects/:slug" element={<ProjectDetail />} />
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </motion.div>
       </AnimatePresence>
       <Footer />
     </>
